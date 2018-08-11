193fa1980ec05e76bd92a8c012f8241dce48278f
Adam Woodcock <adamwoodcock@mbp-2344>
2018-08-11 14:43:39 +0100

    Convert to MVC design pattern

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  Todoey
+//
+//  Created by Adam Woodcock on 11/08/2018.
+//  Copyright © 2018 Adam Woodcock. All rights reserved.
+//
+
+import Foundation
+
+class Item  {
+    var title : String = ""
+    var done : Bool = false
+    
+}
